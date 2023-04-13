d341f77a87f607811ab770c72aba70efe942d38e
Emanuel-Sifuentes <emanuelsifuentes@outlook.com>
2023-04-12 21:57:09 -0400

    uploaded web install file

--- Files/webApp.sh
+++ Files/webApp.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This command is supposed to be run as root, or by a Custom Script Extension if deployed on an Azure VM
 
-apt-get update -y && apt-get install -y python3-pip python3-dev build-essential curl libssl1.1 libssl-dev libpq-dev python-dev
+sudo apt-get -y update
+sudo apt-get -y install python3-pip python3-dev build-essential curl libssl1.1 libssl-dev libpq-dev python-dev
 
 #parameters
 export SQL_SERVER_USERNAME=$1
@@ -11,12 +12,14 @@ export SQL_SERVER_FQDN=$4
 export PORT=$5
 
 cd
-apt-get update -y --fix-missing
+sudo apt-get update -y --fix-missing
 
 wget https://raw.githubusercontent.com/erjosito/whoami/master/api/sql_api.py
 wget https://raw.githubusercontent.com/erjosito/whoami/master/api/requirements.txt
-pip3 install -r requirements.txt
+sudo chmod 777 sql_api.py
+sudo chmod 777 requirements.txt
+sudo pip3 install -r requirements.txt
 
 # Run app
 cd
-nohup python3 sql_api.py &
+sudo nohup python3 sql_api.py &
