97a12f9834bb4b6e64b7ea0023f37ddef91a5885
Emanuel-Sifuentes <emanuelsifuentes@outlook.com>
2023-04-12 21:20:18 -0400

    uploaded web install file

--- Files/webApp.sh
+++ Files/webApp.sh
@@ -1,31 +1,22 @@
 #!/bin/sh
 # This command is supposed to be run as root, or by a Custom Script Extension if deployed on an Azure VM
-# Parameters
-
-SQL_SERVER_USERNAME=$1
-SQL_SERVER_PASSWORD=$2
-SQL_SERVER_DB=$3
-SQL_SERVER_FQDN=$4
-PORT=$5
 
 apt-get update -y && apt-get install -y python3-pip python3-dev build-essential curl libssl1.1 libssl-dev libpq-dev python-dev
 
-# See about installing ODBC drivers here: https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-2017
-# Note that the driver version installed needs to match the version used in the code
+#parameters
+export SQL_SERVER_USERNAME=$1
+export SQL_SERVER_PASSWORD=$2
+export SQL_SERVER_DB=$3
+export SQL_SERVER_FQDN=$4
+export PORT=$5
 
-# Ubuntu 18.04 (ODBC SQL driver 17.0)
 cd
-#curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-#curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update -y --fix-missing
-#ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev mssql-tools
 
 wget https://raw.githubusercontent.com/erjosito/whoami/master/api/sql_api.py
 wget https://raw.githubusercontent.com/erjosito/whoami/master/api/requirements.txt
 pip3 install -r requirements.txt
 
 # Run app
-# python3 sql_api.py &
-# setsid python3 sql_api.py >/dev/null 2>&1
 cd
 nohup python3 sql_api.py &
